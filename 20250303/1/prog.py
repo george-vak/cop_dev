5f88477862f3f349abd480d92918b58296f18734
Vakulenko Georgy <g.vakulenko.s70@gmail.com>
2025-03-09 14:57:40 +0300

    считывание по-новому

--- 20250303/1/prog.py
+++ 20250303/1/prog.py
@@ -1,17 +1,18 @@
 import cowsay
-
+import shlex
 
 field = [[0 for j in range(10)] for i in range(10)]
 allowed_list = cowsay.char_names
 
 def encounter(x, y):
-    out = field[y][x].split()
-    cow_function = getattr(cowsay, out[0])
-    cow_function(out[1])
+
+    cow_function = getattr(cowsay, field[y][x]["name"])
+    cow_function(field[y][x]["word"])
 
 x, y = 0, 0
 while inp := input():
-    inp = inp.split()
+    inp = shlex.split(inp)
+    # print(inp)
 
     moved = 0
     if inp[0] == 'up':
@@ -26,38 +27,60 @@ while inp := input():
     elif inp[0] == 'left':
         x = (x - 1) % 10
         moved = 1
-
     if moved == 1:
         print(f'Moved to ({x}, {y})')
 
         if field[y][x] != 0:
             encounter(x, y)
+
     else:
         if inp[0] == 'addmon':
-            if len(inp) < 5:
-                print("Invalid arguments")
+            if len(inp) != 9:
+                print("Invalid arguments <<1>>")
                 continue
 
-            try:
-                m_x = int(inp[2])
-                m_y = int(inp[3])
-
-                if m_x < 0 or m_x > 9 or m_y < 0 or m_y > 9:
-                    raise Exception
-            except Exception:
-                print("Invalid arguments")
-                continue
-
-            if inp[1] not in allowed_list:
+            curr_name = inp[1]
+            if curr_name not in allowed_list:
                 print("Cannot add unknown monster")
                 continue
 
+            m_x, m_y, curr_hp = 0, 0, 0
+            curr_word = ""
+            ii = 2
+            while ii < 9:
+                match inp[ii]:
+                    case "hello":
+                        curr_word = inp[ii+1]
+                        ii += 1
+
+                    case "hp":
+                        try:
+                            curr_hp = int(inp[ii+1])
+                        except Exception:
+                            break
+                        if curr_hp < 1:
+                            break
+                        ii += 1
+
+                    case "coords":
+                        try:
+                            m_x = int(inp[ii+1])
+                            m_y = int(inp[ii+2])
+                        except Exception:
+                            break
+                        if m_x < 0 or m_x > 9 or m_y < 0 or m_y > 9:
+                            break
+                        ii += 2
+                ii += 1
+
+
             if field[m_y][m_x] == 0:
-                print(f'Added monster {inp[1]} to ({m_x}, {m_y}) saying {inp[4]}')
+                print(f'Added monster {curr_name} to ({m_x}, {m_y}) saying {curr_word}')
             else:
                 print(f'Replaced the old monster')
 
-            field[m_y][m_x] = inp[1] + ' ' + inp[4]
 
         else:
             print('Invalid command')
+
+# addmon tux hello SIUUUU hp 120 coords 0 1
