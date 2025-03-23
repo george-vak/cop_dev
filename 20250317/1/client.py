d15eb476f28ad51fb3e695fdc8c28d6a6692dfeb
Vakulenko Georgy <g.vakulenko.s70@gmail.com>
2025-03-23 21:49:14 +0300

    атаки клиент+сервер

--- 20250317/1/client.py
+++ 20250317/1/client.py
@@ -1,11 +1,7 @@
 import cmd
 from io import StringIO
-from math import trunc
-
 import cowsay
 import shlex
-
-
 import socket
 
 class MUD(cmd.Cmd):
@@ -18,8 +14,6 @@ class MUD(cmd.Cmd):
 
         self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.client_socket.connect(('127.0.0.1', 12345))
-
-        # self.allowed_list = ["cow"]
 
         self.jgsbat = cowsay.read_dot_cow(StringIO("""
         $the_cow = <<EOC;
@@ -36,30 +30,19 @@ class MUD(cmd.Cmd):
         """))
         self.allowed_list = cowsay.list_cows() + ["jgsbat"]
 
-    # def encounter(self):
-    #     if self.field[self.y][self.x] == 0:
-    #         print(f'Moved to ({self.x}, {self.y})')
-    #         return
-    #
-    #     name = self.field[self.y][self.x]['name']
-    #     word = self.field[self.y][self.x]['word']
-    #
-    #     if name == "jgsbat":
-    #         print(cowsay.cowsay(word, cowfile=self.jgsbat))
-    #     else:
-    #         print(cowsay.cowsay(word, cow=name))
-    
+
     def encounter(self, name, word):
         if name == "jgsbat":
             print(cowsay.cowsay(word, cowfile=self.jgsbat))
         else:
             print(cowsay.cowsay(word, cow=name))
-    
-    
+
+
     def send_comm(self, comm):
         self.client_socket.send(comm.encode())
         ans = self.client_socket.recv(1024).decode()
         return ans
+
 
     def do_up(self, arg):
         answ = self.send_comm("move 0 -1")
@@ -68,7 +51,8 @@ class MUD(cmd.Cmd):
             print(f'Moved to ({x}, {y})')
         except ValueError:
             self.encounter(answ.split()[0], answ.split()[1])
-            
+
+
     def do_down(self, arg):
         answ = self.send_comm("move 0 1")
         try:
@@ -76,7 +60,8 @@ class MUD(cmd.Cmd):
             print(f'Moved to ({x}, {y})')
         except ValueError:
             self.encounter(answ.split()[0], answ.split()[1])
-            
+
+
     def do_right(self, arg):
         answ = self.send_comm("move 1 0")
         try:
@@ -84,6 +69,7 @@ class MUD(cmd.Cmd):
             print(f'Moved to ({x}, {y})')
         except ValueError:
             self.encounter(answ.split()[0], answ.split()[1])
+
 
     def do_left(self, arg):
         answ = self.send_comm("move -1 0")
@@ -93,13 +79,13 @@ class MUD(cmd.Cmd):
         except ValueError:
             self.encounter(answ.split()[0], answ.split()[1])
 
+
     def do_exit(self, arg):
         self.client_socket.send(b"exit")
         self.client_socket.close()
         print("Disconnect.")
         return True
 
-# addmon cow hp 93 hello SJSJSJS coords 0 1
 
     def do_addmon(self, arg):
         inp = shlex.split(arg)
@@ -144,71 +130,60 @@ class MUD(cmd.Cmd):
                         return
                     ii += 2
 
-
             ii += 1
 
         print(self.send_comm(f"add {curr_name} {m_x} {m_y} {curr_word} {curr_hp}"))
 
 
+    def do_attack(self, arg=""):
+        if not arg:
+            print("Укажите имя монстра для атаки")
+            return
+        else:
+            arg = shlex.split(arg)
+        arg.append("sword")
 
-        # if self.field[m_y][m_x] == 0:
-        #     print(f'Added monster {curr_name} to ({m_x}, {m_y}) saying {curr_word}')
-        # else:
-        #     print(f'Replaced the old monster')
-        #
-        # self.field[m_y][m_x] = {'name': curr_name, 'word': curr_word, 'hp': curr_hp}
+        if arg[1] == "with":
+            if arg[2] in self.arsenal:
+                weap = arg[2]
+            else:
+                print("Unknown weapon")
+                return
+        elif arg[1] == "sword":
+            weap = arg[1]
+        else:
+            print("ожидается: attack <mons_name> with <weap_name>")
+            return
 
-    # def do_attack(self, arg=""):
-    #     if self.field[self.y][self.x] == 0:
-    #         print(f"No monster here")
-    #         return
-    #
-    #     if not arg:
-    #         print("Укажите имя монстра для атаки")
-    #         return
-    #     else:
-    #         arg = shlex.split(arg)
-    #     arg.append("sword")
-    #
-    #     if arg[0] != self.field[self.y][self.x]["name"]:
-    #         print(f"No {arg[0]} here")
-    #         return
-    #
-    #     if arg[1] == "with":
-    #         if arg[2] in self.arsenal:
-    #             weap = arg[2]
-    #         else:
-    #             print("Unknown weapon")
-    #             return
-    #     elif arg[1] == "sword":
-    #         weap = arg[1]
-    #     else:
-    #         print("ожидается: attack <mons_name> with <weap_name>")
-    #         return
-    #
-    #
-    #     if self.field[self.y][self.x]["hp"] <= self.arsenal[weap]:
-    #         print(f"{self.field[self.y][self.x]['name']} died")
-    #         self.field[self.y][self.x] = 0
-    #         return
-    #
-    #     else:
-    #         self.field[self.y][self.x]["hp"] -= self.arsenal[weap]
-    #         print(f"{self.field[self.y][self.x]['name']} now has {
-    #         self.field[self.y][self.x]['hp']}")
-    #
-    # def complete_attack(self, text, line, begidx, endidx):
-    #     words = shlex.split(line[:endidx])
-    #     if len(words) == 1:
-    #         return self.allowed_list
-    #     elif len(words) == 2:
-    #         return [mons for mons in self.allowed_list if mons.startswith(text)]
-    #
-    #     elif len(words) == 3 and words[2] == "with":
-    #         return list(self.arsenal)
-    #     elif len(words) == 4 and words[2] == "with":
-    #         return [weap for weap in self.arsenal if weap.startswith(text)]
-    #     return []
+        answ = self.send_comm(f"attack {arg[0]} {self.arsenal[weap]}").split()
+        # name hp hit
+        if answ[0] == "No":
+            print(*answ)
+        else:
+            print(f"Attacked {arg[0]}, damage {answ[2]} hp")
+            if answ[1] == "0":
+                print(f"{arg[0]} died")
+            else:
+                print(f"{arg[0]} now has {answ[1]} hp")
+
+
+    def complete_attack(self, text, line, begidx, endidx):
+        words = shlex.split(line[:endidx])
+        if len(words) == 1:
+            return self.allowed_list
+        elif len(words) == 2:
+            return [mons for mons in self.allowed_list if mons.startswith(text)]
+
+        elif len(words) == 3 and words[2] == "with":
+            return list(self.arsenal)
+        elif len(words) == 4 and words[2] == "with":
+            return [weap for weap in self.arsenal if weap.startswith(text)]
+        return []
+
+
+    def do_help(self, arg):
+        print("-----\naddmon tux hp 25 hello SJSJSJS coords 0 0")
+        print("attack tux with axe\n-----")
 
 if __name__ == "__main__":
     MUD().cmdloop()
